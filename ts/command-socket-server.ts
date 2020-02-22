4c35527e365885e0e7caa1c7503c604ae80cb436
Trevor Sears <trevorsears.main@gmail.com>
2020-02-22 16:32:49 -0500

    Added metadata types for underlying CommandSockets.

--- ts/command-socket-server.ts
+++ ts/command-socket-server.ts
@@ -21,12 +21,13 @@ import { CommandSocket } from "@command-socket/node-client";
  */
 export class CommandSocketServer<
 	LCS extends CommandSetStructure = any,
-	RCS extends CommandSetStructure = any> {
+	RCS extends CommandSetStructure = any,
+	M extends {} = {}> {
 	
 	/**
 	 * A mapping from CommandSocket IDs to CommandSockets, including all currently connected clients.
 	 */
-	private connectionMap: Map<string, CommandSocket<LCS, RCS>>;
+	private connectionMap: Map<string, CommandSocket<LCS, RCS, M>>;
 	
 	// DOC-ME [1/13/20 @ 12:14 PM] - Documentation required!
 	private internalServer: WebSocketServer;
@@ -43,7 +44,7 @@ export class CommandSocketServer<
 	 */
 	public constructor(port: number, commandRegistry?: CommandRegistry<LCS>) {
 	
-		this.connectionMap = new Map<string, CommandSocket<LCS, RCS>>();
+		this.connectionMap = new Map<string, CommandSocket<LCS, RCS, M>>();
 		
 		this.internalServer = new WebSocketServer({ port });
 		
@@ -51,14 +52,14 @@ export class CommandSocketServer<
 		
 		this.internalServer.on("connection", async (websocket: WebSocket): Promise<void> => {
 			
-			let connection: CommandSocket<LCS, RCS> = await CommandSocket.create(websocket, commandRegistry);
+			let connection: CommandSocket<LCS, RCS, M> = await CommandSocket.create(websocket, commandRegistry);
 			
 			this.connectionMap.set(connection.getID(), connection);
 			
 			// FIX-ME [11/26/19 @ 1:58 AM] - This is not the correct way to fix the below issue...
 			this.getEvents().CONNECTION_OPENED.notify(connection as unknown as CommandSocket);
 			
-			connection.getEvents().CLOSE.subscribe((event: { source: CommandSocket }) => {
+			connection.getEvents().CLOSE.subscribe((event: { source: CommandSocket }): void => {
 				
 				this.connectionMap.delete(event.source.getID());
 				
@@ -81,7 +82,7 @@ export class CommandSocketServer<
 	}
 	
 	// DOC-ME [1/13/20 @ 12:14 PM] - Documentation required!
-	public getConnectionForID(id: string): CommandSocket<LCS, RCS> | undefined {
+	public getConnectionForID(id: string): CommandSocket<LCS, RCS, M> | undefined {
 		
 		return this.connectionMap.get(id);
 		
@@ -96,7 +97,7 @@ export class CommandSocketServer<
 	}
 	
 	// DOC-ME [1/13/20 @ 12:14 PM] - Documentation required!
-	public forEachConnection(callback: (connection: CommandSocket<LCS, RCS>) => any): void {
+	public forEachConnection(callback: (connection: CommandSocket<LCS, RCS, M>) => any): void {
 		
 		for (let connection of this.connectionMap.values()) callback(connection);
 		
@@ -118,7 +119,7 @@ export class CommandSocketServer<
 	 */
 	public close(): void {
 		
-		this.forEachConnection((connection: CommandSocket<LCS, RCS>) => connection.close());
+		this.forEachConnection((connection: CommandSocket<LCS, RCS, M>): any => connection.close());
 		this.internalServer.close();
 		
 	}
