52a40513a7a3c34de7acbe0b9b3f3433be4a52d8
Trevor Sears <trevorsears.main@gmail.com>
2020-02-22 16:33:29 -0500

    Added compiled code for v0.4.1.

--- .d.ts/command-socket-server.d.ts
+++ .d.ts/command-socket-server.d.ts
@@ -1,15 +1,15 @@
 import { AventNotifier } from "avents";
 import { CommandRegistry, CommandSetStructure } from "@command-socket/core";
 import { CommandSocket } from "@command-socket/node-client";
-export declare class CommandSocketServer<LCS extends CommandSetStructure = any, RCS extends CommandSetStructure = any> {
+export declare class CommandSocketServer<LCS extends CommandSetStructure = any, RCS extends CommandSetStructure = any, M extends {} = {}> {
     private connectionMap;
     private internalServer;
     private readonly events;
     constructor(port: number, commandRegistry?: CommandRegistry<LCS>);
     hasConnectionForID(id: string): boolean;
-    getConnectionForID(id: string): CommandSocket<LCS, RCS> | undefined;
+    getConnectionForID(id: string): CommandSocket<LCS, RCS, M> | undefined;
     getCommandRegistry(): any;
-    forEachConnection(callback: (connection: CommandSocket<LCS, RCS>) => any): void;
+    forEachConnection(callback: (connection: CommandSocket<LCS, RCS, M>) => any): void;
     getEvents(): CommandSocketServerEvents;
     close(): void;
 }
